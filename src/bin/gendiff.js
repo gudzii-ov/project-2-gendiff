10247057a8a6a63bd4cb6f979b8beaaf1aea98c8
Oleg Gduzii <oleg198679@gmail.com>
2018-06-25 11:35:08 +0300

    Add options help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+const program = require('commander');
+
+program
+  .version('0.0.3')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
