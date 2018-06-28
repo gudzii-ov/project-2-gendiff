87d25532583bf26b82395edafbc2fc63f2327d01
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 18:07:51 +0300

    Add yaml test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,14 +2,28 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const pathToFile1 = './__tests__/__fixtures__/before.json';
-const pathToFile2 = './__tests__/__fixtures__/after.json';
 const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
-
 const results = JSON.parse(fs.readFileSync(pathToResults));
 
-const expectedJSON = results['json-diff'];
+const pathToJSON1 = './__tests__/__fixtures__/before.json';
+const pathToJSON2 = './__tests__/__fixtures__/after.json';
 
-test('compares 2 json files', () => {
-  expect(genDiff(pathToFile1, pathToFile2)).toBe(expectedJSON);
+const pathToYAML1 = './__tests__/__fixtures__/before.yaml';
+const pathToYAML2 = './__tests__/__fixtures__/after.yaml';
+const pathToYML1 = './__tests__/__fixtures__/before.yml';
+const pathToYML2 = './__tests__/__fixtures__/after.yml';
+
+const expectedJSON = results['json-diff'];
+const expectedYAML = results['yaml-diff'];
+
+test('compares 2 JSON configs', () => {
+  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expectedJSON);
+});
+
+test('compares 2 YAML configs', () => {
+  expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expectedYAML);
+});
+
+test('compares 2 YML configs', () => {
+  expect(genDiff(pathToYML1, pathToYML2)).toBe(expectedYAML);
 });
