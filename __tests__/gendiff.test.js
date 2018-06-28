6d9b954fe4f5cb1e9958143bbd1fc506db309a0e
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 18:31:56 +0300

    Add ini tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,17 +13,23 @@ const pathToYAML2 = './__tests__/__fixtures__/after.yaml';
 const pathToYML1 = './__tests__/__fixtures__/before.yml';
 const pathToYML2 = './__tests__/__fixtures__/after.yml';
 
-const expectedJSON = results['json-diff'];
-const expectedYAML = results['yaml-diff'];
+const pathToINI1 = './__tests__/__fixtures__/before.ini';
+const pathToINI2 = './__tests__/__fixtures__/after.ini';
+
+const expectedFlat = results['flat-diff'];
 
 test('compares 2 JSON configs', () => {
-  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expectedJSON);
+  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expectedFlat);
 });
 
 test('compares 2 YAML configs', () => {
-  expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expectedYAML);
+  expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expectedFlat);
 });
 
 test('compares 2 YML configs', () => {
-  expect(genDiff(pathToYML1, pathToYML2)).toBe(expectedYAML);
+  expect(genDiff(pathToYML1, pathToYML2)).toBe(expectedFlat);
+});
+
+test('compares 2 INI configs', () => {
+  expect(genDiff(pathToINI1, pathToINI2)).toBe(expectedFlat);
 });
