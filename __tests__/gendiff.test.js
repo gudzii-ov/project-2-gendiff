58d49a6f977409040e0e8261f6bd4adbd20642a4
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 15:34:05 +0300

    Fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,9 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const pathToFile1 = path.join(__dirname, '__fixtures__', 'before.json');
-const pathToFile2 = path.join(__dirname, '__fixtures__', 'after.json');
-const pathToResults = path.join(__dirname, '__fixtures__', 'results.json');
+// const pathToFile1 = path.resolve(__dirname, '__fixtures__', 'before.json');
+// const pathToFile2 = path.resolve(__dirname, '__fixtures__', 'after.json');
+const pathToFile1 = './__tests__/__fixtures__/before.json';
+const pathToFile2 = './__tests__/__fixtures__/after.json';
+const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
 
 const results = JSON.parse(fs.readFileSync(pathToResults));
 
