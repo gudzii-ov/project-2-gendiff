cd646d7c0f3a6fd9ba114a1861914a3fe3a4cd62
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 14:38:12 +0300

    Refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,9 +4,12 @@ import genDiff from '../src';
 
 const pathToFile1 = path.join(__dirname, '__fixtures__', 'before.json');
 const pathToFile2 = path.join(__dirname, '__fixtures__', 'after.json');
+const pathToResults = path.join(__dirname, '__fixtures__', 'results.json');
 
-const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
+const results = JSON.parse(fs.readFileSync(pathToResults));
+
+const expectedJSON = results['json-diff'];
 
 test('compares 2 json files', () => {
-  expect(genDiff(pathToFile1, pathToFile2)).toBe(expected);
+  expect(genDiff(pathToFile1, pathToFile2)).toBe(expectedJSON);
 });
