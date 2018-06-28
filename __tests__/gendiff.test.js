3fe93e7d20d2e2d5db7cb48e199ebe1ea5febc58
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 15:38:43 +0300

    Fix version. Remove unnecessary comments

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,8 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-// const pathToFile1 = path.resolve(__dirname, '__fixtures__', 'before.json');
-// const pathToFile2 = path.resolve(__dirname, '__fixtures__', 'after.json');
 const pathToFile1 = './__tests__/__fixtures__/before.json';
 const pathToFile2 = './__tests__/__fixtures__/after.json';
 const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
