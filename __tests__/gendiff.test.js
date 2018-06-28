c0b555cb44ef10ef5c02c88b1d8f90beb1078bf5
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 14:32:19 +0300

    Fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,11 @@
+import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
 const pathToFile1 = path.join(__dirname, '__fixtures__', 'before.json');
 const pathToFile2 = path.join(__dirname, '__fixtures__', 'after.json');
 
-const expected = '{host: hexlet.io + timeout: 20 - timeout: 50 - proxy: 123.234.53.22 + verbose: true - follow: false}';
+const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
 
 test('compares 2 json files', () => {
   expect(genDiff(pathToFile1, pathToFile2)).toBe(expected);
