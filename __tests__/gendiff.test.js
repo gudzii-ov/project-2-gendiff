8e80a98f7e85dab909ca340ff69a302b1d869470
Oleg Gduzii <oleg198679@gmail.com>
2018-06-28 22:20:44 +0300

    Test refactor #3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,16 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const pathToConfig = './__tests__/__fixtures__';
-const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
-
 describe('Compares flat config files', () => {
+  const pathToConfig = './__tests__/__fixtures__';
+
+  const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
+  const results = JSON.parse(fs.readFileSync(pathToResults));
+  const expectedFlat = results['flat-diff'];
+
   test('compares 2 JSON configs', () => {
     const pathToJSON1 = `${pathToConfig}/before.json`;
     const pathToJSON2 = `${pathToConfig}/after.json`;
-
-    const results = JSON.parse(fs.readFileSync(pathToResults));
-    const expectedFlat = results['flat-diff'];
 
     expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expectedFlat);
   });
@@ -20,9 +20,6 @@ describe('Compares flat config files', () => {
     const pathToYAML1 = `${pathToConfig}/before.yaml`;
     const pathToYAML2 = `${pathToConfig}/after.yaml`;
 
-    const results = JSON.parse(fs.readFileSync(pathToResults));
-    const expectedFlat = results['flat-diff'];
-
     expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expectedFlat);
   });
 
@@ -30,18 +27,12 @@ describe('Compares flat config files', () => {
     const pathToYML1 = `${pathToConfig}/before.yml`;
     const pathToYML2 = `${pathToConfig}/after.yml`;
 
-    const results = JSON.parse(fs.readFileSync(pathToResults));
-    const expectedFlat = results['flat-diff'];
-
     expect(genDiff(pathToYML1, pathToYML2)).toBe(expectedFlat);
   });
 
   test('compares 2 INI configs', () => {
     const pathToINI1 = `${pathToConfig}/before.ini`;
     const pathToINI2 = `${pathToConfig}/after.ini`;
-
-    const results = JSON.parse(fs.readFileSync(pathToResults));
-    const expectedFlat = results['flat-diff'];
 
     expect(genDiff(pathToINI1, pathToINI2)).toBe(expectedFlat);
   });
