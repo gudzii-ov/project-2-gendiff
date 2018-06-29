43f66bb1ce5bf3986c3e49961456b53440243205
Oleg Gduzii <oleg198679@gmail.com>
2018-06-29 12:55:53 +0300

    Refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,35 +5,35 @@ import genDiff from '../src';
 describe('Compares flat config files', () => {
   const pathToConfig = './__tests__/__fixtures__';
 
-  const pathToResults = path.resolve(__dirname, '__fixtures__', 'results.json');
-  const results = JSON.parse(fs.readFileSync(pathToResults));
-  const expectedFlat = results['flat-diff'];
-
   test('compares 2 JSON configs', () => {
     const pathToJSON1 = `${pathToConfig}/before.json`;
     const pathToJSON2 = `${pathToConfig}/after.json`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-json'), 'utf-8');
 
-    expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expectedFlat);
+    expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expected);
   });
 
   test('compares 2 YAML configs', () => {
     const pathToYAML1 = `${pathToConfig}/before.yaml`;
     const pathToYAML2 = `${pathToConfig}/after.yaml`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-yaml'), 'utf-8');
 
-    expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expectedFlat);
+    expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expected);
   });
 
   test('compares 2 YML configs', () => {
     const pathToYML1 = `${pathToConfig}/before.yml`;
     const pathToYML2 = `${pathToConfig}/after.yml`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-yaml'), 'utf-8');
 
-    expect(genDiff(pathToYML1, pathToYML2)).toBe(expectedFlat);
+    expect(genDiff(pathToYML1, pathToYML2)).toBe(expected);
   });
 
   test('compares 2 INI configs', () => {
     const pathToINI1 = `${pathToConfig}/before.ini`;
     const pathToINI2 = `${pathToConfig}/after.ini`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-ini'), 'utf-8');
 
-    expect(genDiff(pathToINI1, pathToINI2)).toBe(expectedFlat);
+    expect(genDiff(pathToINI1, pathToINI2)).toBe(expected);
   });
 });
