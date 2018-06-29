dd3a2f115cf5cbaf6b907d73ac6064e40d794a93
Oleg Gduzii <oleg198679@gmail.com>
2018-06-29 19:09:53 +0300

    Tests refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,33 +6,33 @@ describe('Compares flat config files', () => {
   const pathToConfig = './__tests__/__fixtures__';
 
   test('compares 2 JSON configs', () => {
-    const pathToJSON1 = `${pathToConfig}/before.json`;
-    const pathToJSON2 = `${pathToConfig}/after.json`;
-    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-json'), 'utf-8');
+    const pathToJSON1 = `${pathToConfig}/json/before.json`;
+    const pathToJSON2 = `${pathToConfig}/json/after.json`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'json', 'expected-json'), 'utf-8');
 
     expect(genDiff(pathToJSON1, pathToJSON2)).toBe(expected);
   });
 
   test('compares 2 YAML configs', () => {
-    const pathToYAML1 = `${pathToConfig}/before.yaml`;
-    const pathToYAML2 = `${pathToConfig}/after.yaml`;
-    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-yaml'), 'utf-8');
+    const pathToYAML1 = `${pathToConfig}/yaml/before.yaml`;
+    const pathToYAML2 = `${pathToConfig}/yaml/after.yaml`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected-yaml'), 'utf-8');
 
     expect(genDiff(pathToYAML1, pathToYAML2)).toBe(expected);
   });
 
   test('compares 2 YML configs', () => {
-    const pathToYML1 = `${pathToConfig}/before.yml`;
-    const pathToYML2 = `${pathToConfig}/after.yml`;
-    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-yaml'), 'utf-8');
+    const pathToYML1 = `${pathToConfig}/yaml/before.yml`;
+    const pathToYML2 = `${pathToConfig}/yaml/after.yml`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected-yaml'), 'utf-8');
 
     expect(genDiff(pathToYML1, pathToYML2)).toBe(expected);
   });
 
   test('compares 2 INI configs', () => {
-    const pathToINI1 = `${pathToConfig}/before.ini`;
-    const pathToINI2 = `${pathToConfig}/after.ini`;
-    const expected = fs.readFileSync(path.resolve(pathToConfig, 'expected-ini'), 'utf-8');
+    const pathToINI1 = `${pathToConfig}/ini/before.ini`;
+    const pathToINI2 = `${pathToConfig}/ini/after.ini`;
+    const expected = fs.readFileSync(path.resolve(pathToConfig, 'ini', 'expected-ini'), 'utf-8');
 
     expect(genDiff(pathToINI1, pathToINI2)).toBe(expected);
   });
