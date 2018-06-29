5df1de35b44e49e32d6ff4e9e154a66777989229
Oleg Gduzii <oleg198679@gmail.com>
2018-06-29 22:15:13 +0300

    Add build ast for flat configs

--- src/index.js
+++ src/index.js
@@ -3,21 +3,46 @@ import path from 'path';
 import _ from 'lodash';
 import getParser from './parser';
 
-const getKeyString = (key, data1, data2) => {
-  if (!(_.has(data2, key))) {
-    return `- ${key}: ${data1[key]}`;
-  }
+const buildAst = (data1, data2) => {
+  const buildNode = (key, type, beforeValue, afterValue, children = []) =>
+    ({
+      key, type, beforeValue, afterValue, children,
+    });
 
-  if (!(_.has(data1, key))) {
-    return `+ ${key}: ${data2[key]}`;
-  }
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
 
-  if (data1[key] === data2[key]) {
-    return `  ${key}: ${data1[key]}`;
-  }
+  return keys.map((key) => {
+    if (!(_.has(data2, key))) {
+      return buildNode(key, 'removed', data1[key]);
+    }
 
-  return `+ ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}`;
+    if (!(_.has(data1, key))) {
+      return buildNode(key, 'added', undefined, data2[key]);
+    }
+
+    if (data1[key] === data2[key]) {
+      return buildNode(key, 'unchanged', data1[key]);
+    }
+
+    return buildNode(key, 'changed', data1[key], data2[key]);
+  });
 };
+
+// const getKeyString = (key, data1, data2) => {
+//   if (!(_.has(data2, key))) {
+//     return `- ${key}: ${data1[key]}`;
+//   }
+//
+//   if (!(_.has(data1, key))) {
+//     return `+ ${key}: ${data2[key]}`;
+//   }
+//
+//   if (data1[key] === data2[key]) {
+//     return `  ${key}: ${data1[key]}`;
+//   }
+//
+//   return `+ ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}`;
+// };
 
 const getFormat = pathToFile => path.extname(pathToFile);
 
@@ -31,10 +56,15 @@ const genDiff = (pathToFile1, pathToFile2) => {
   const data1 = getParser(format1).parse(rawData1);
   const data2 = getParser(format2).parse(rawData2);
 
-  const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const ast = buildAst(data1, data2);
 
-  return `${keys.reduce((acc, cKey) =>
-    `${acc}  ${getKeyString(cKey, data1, data2)}\n`, '{\n')}}`;
+  console.log(ast);
+  return ast;
+
+  // const keys = _.union(Object.keys(data1), Object.keys(data2));
+  //
+  // return `${keys.reduce((acc, cKey) =>
+  //   `${acc}  ${getKeyString(cKey, data1, data2)}\n`, '{\n')}}`;
 };
 
 export default genDiff;
