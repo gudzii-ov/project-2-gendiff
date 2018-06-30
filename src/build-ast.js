280411022ae1be95111bc8f7e714f5d12756c732
Oleg Gduzii <oleg198679@gmail.com>
2018-06-30 20:22:27 +0300

    Fix ast build

--- src/build-ast.js
+++ src/build-ast.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 
-const buildNode = (key, type, beforeValue, afterValue, children = []) =>
+const buildKeyObject = (key, type, valueBefore, valueAfter, children = []) =>
   ({
-    key, type, beforeValue, afterValue, children,
+    name: key, type, valueBefore, valueAfter, children,
   });
 
 const buildAst = (data1, data2) => {
@@ -10,31 +10,27 @@ const buildAst = (data1, data2) => {
 
   return keys.map((key) => {
     if (!(_.has(data2, key))) {
-      return buildNode(key, 'removed', data1[key]);
+      return buildKeyObject(key, 'removed', data1[key]);
     }
 
     if (!(_.has(data1, key))) {
-      return buildNode(key, 'added', undefined, data2[key]);
+      return buildKeyObject(key, 'added', undefined, data2[key]);
     }
 
     if (data1[key] instanceof Object && data2[key] instanceof Object) {
-      if (_.isEqual(data1[key], data2[key])) {
-        return buildNode(key, 'unchanged', data1[key]);
-      }
-
       const subData1 = data1[key];
       const subData2 = data2[key];
 
       const child = buildAst(subData1, subData2);
 
-      return buildNode(key, 'unchanged', undefined, undefined, child);
+      return buildKeyObject(key, 'nested', undefined, undefined, child);
     }
 
     if (data1[key] === data2[key]) {
-      return buildNode(key, 'unchanged', data1[key]);
+      return buildKeyObject(key, 'unchanged', data1[key]);
     }
 
-    return buildNode(key, 'changed', data1[key], data2[key]);
+    return buildKeyObject(key, 'changed', data1[key], data2[key]);
   });
 };
 
