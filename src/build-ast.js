f3bd0e5ad236149526653a0ed2ae9bba417eb53f
Oleg Gduzii <oleg198679@gmail.com>
2018-06-30 09:59:41 +0300

    Add build ast for recursive configs

--- src/build-ast.js
+++ src/build-ast.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
-export default (data1, data2) => {
-  const buildNode = (key, type, beforeValue, afterValue, children = []) =>
-    ({
-      key, type, beforeValue, afterValue, children,
-    });
+const buildNode = (key, type, beforeValue, afterValue, children = []) =>
+  ({
+    key, type, beforeValue, afterValue, children,
+  });
 
+const buildAst = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
 
   return keys.map((key) => {
@@ -17,6 +17,19 @@ export default (data1, data2) => {
       return buildNode(key, 'added', undefined, data2[key]);
     }
 
+    if (data1[key] instanceof Object && data2[key] instanceof Object) {
+      if (_.isEqual(data1[key], data2[key])) {
+        return buildNode(key, 'unchanged', data1[key]);
+      }
+
+      const subData1 = data1[key];
+      const subData2 = data2[key];
+
+      const child = buildAst(subData1, subData2);
+
+      return buildNode(key, 'unchanged', undefined, undefined, child);
+    }
+
     if (data1[key] === data2[key]) {
       return buildNode(key, 'unchanged', data1[key]);
     }
@@ -24,3 +37,5 @@ export default (data1, data2) => {
     return buildNode(key, 'changed', data1[key], data2[key]);
   });
 };
+
+export default buildAst;
