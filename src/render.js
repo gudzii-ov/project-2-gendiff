dbaca2af2dcbc73764bf00d28cc3c8672b7a46d7
Oleg Gduzii <oleg198679@gmail.com>
2018-06-30 15:32:14 +0300

    Add stringify. WIP

--- src/render.js
+++ src/render.js
@@ -1,3 +1,19 @@
+const indent = ' '.repeat(2);
+
+const stringify = (node, depth) => {
+  if (!(node instanceof Object)) {
+    return node;
+  }
+
+  const nodeKeys = Object.keys(node);
+  const string = nodeKeys.map(cKey =>
+    `${indent.repeat(depth + 1)}${cKey}: ${stringify(node[cKey], depth + 1)}`).join('\n');
+
+  const result = ['{', string, `${indent.repeat(depth)}}`].join('\n');
+
+  return result;
+};
+
 const keyStatus = {
   unchanged: '  ',
   added: '+ ',
@@ -6,22 +22,24 @@ const keyStatus = {
 
 const keyString = {
   unchanged: (key, depth) =>
-    `${' '.repeat(2 * depth)}${keyStatus.unchanged}${key.key}: ${key.beforeValue}`,
+    `${indent.repeat(depth)}${keyStatus.unchanged}${key.key}: ${key.beforeValue}`,
   changed: (key, depth) =>
-    `${' '.repeat(2 * depth)}${keyStatus.added}${key.key}: ${key.afterValue}\n${' '.repeat(2 * depth)}${keyStatus.removed}${key.key}: ${key.beforeValue}`,
+    `${indent.repeat(depth)}${keyStatus.added}${key.key}: ${key.afterValue}\n${indent.repeat(depth)}${keyStatus.removed}${key.key}: ${key.beforeValue}`,
   added: (key, depth) =>
-    `${' '.repeat(2 * depth)}${keyStatus.added}${key.key}: ${key.afterValue}`,
+    `${indent.repeat(depth)}${keyStatus.added}${key.key}: ${key.afterValue}`,
   removed: (key, depth) =>
-    `${' '.repeat(2 * depth)}${keyStatus.removed}${key.key}: ${key.beforeValue}`,
+    `${indent.repeat(depth)}${keyStatus.removed}${key.key}: ${key.beforeValue}`,
 };
 
 const getKeyString = type => keyString[type];
 
 const renderNode = (node, depth) => {
   const renderedKeys = node.map(key => getKeyString(key.type)(key, depth + 1));
-  const result = [`${' '.repeat(2 * depth)}{`, ...renderedKeys, `${' '.repeat(2 * depth)}}`];
+  const result = [`${indent.repeat(depth)}{`, ...renderedKeys, `${indent.repeat(depth)}}`];
 
   return result.join('\n');
 };
 
-export default ast => renderNode(ast, 0);
+export default (ast) => {
+  return renderNode(ast, 0);
+};
