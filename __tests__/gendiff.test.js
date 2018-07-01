ab539b4623ae8afb1ee76e79f5c188215f279287
Oleg Gduzii <oleg198679@gmail.com>
2018-07-01 10:29:39 +0300

    Add tests for json output

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -149,3 +149,77 @@ describe('Test plain output', () => {
     });
   });
 });
+
+describe('Test json output', () => {
+  describe('Compares flat config files', () => {
+    const pathToConfig = './__tests__/__fixtures__';
+
+    test('compares 2 JSON configs', () => {
+      const pathToJSON1 = `${pathToConfig}/json/input/before.json`;
+      const pathToJSON2 = `${pathToConfig}/json/input/after.json`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'json', 'expected', 'plain-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToJSON1, pathToJSON2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 YAML configs', () => {
+      const pathToYAML1 = `${pathToConfig}/yaml/input/before.yaml`;
+      const pathToYAML2 = `${pathToConfig}/yaml/input/after.yaml`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected', 'plain-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToYAML1, pathToYAML2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 YML configs', () => {
+      const pathToYML1 = `${pathToConfig}/yaml/input/before.yml`;
+      const pathToYML2 = `${pathToConfig}/yaml/input/after.yml`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected', 'plain-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToYML1, pathToYML2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 INI configs', () => {
+      const pathToINI1 = `${pathToConfig}/ini/input/before.ini`;
+      const pathToINI2 = `${pathToConfig}/ini/input/after.ini`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'ini', 'expected', 'plain-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToINI1, pathToINI2, 'json')).toBe(expected);
+    });
+  });
+
+  describe('Compares recursive config files', () => {
+    const pathToConfig = './__tests__/__fixtures__';
+
+    test('compares 2 JSON configs', () => {
+      const pathToJSON1 = `${pathToConfig}/json/input/before-recursive.json`;
+      const pathToJSON2 = `${pathToConfig}/json/input/after-recursive.json`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'json', 'expected', 'recursive-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToJSON1, pathToJSON2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 YAML configs', () => {
+      const pathToYAML1 = `${pathToConfig}/yaml/input/before-recursive.yaml`;
+      const pathToYAML2 = `${pathToConfig}/yaml/input/after-recursive.yaml`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected', 'recursive-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToYAML1, pathToYAML2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 YML configs', () => {
+      const pathToYML1 = `${pathToConfig}/yaml/input/before-recursive.yml`;
+      const pathToYML2 = `${pathToConfig}/yaml/input/after-recursive.yml`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'yaml', 'expected', 'recursive-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToYML1, pathToYML2, 'json')).toBe(expected);
+    });
+
+    test('compares 2 INI configs', () => {
+      const pathToINI1 = `${pathToConfig}/ini/input/before-recursive.ini`;
+      const pathToINI2 = `${pathToConfig}/ini/input/after-recursive.ini`;
+      const expected = fs.readFileSync(path.resolve(pathToConfig, 'ini', 'expected', 'recursive-in-json-out'), 'utf-8');
+
+      expect(genDiff(pathToINI1, pathToINI2, 'json')).toBe(expected);
+    });
+  });
+});
